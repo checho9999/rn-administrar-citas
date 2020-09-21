e65c61cda897bb06e48125223ae7a922aa15edfe
Checho <checho9999@gmail.com>
2020-09-21 08:52:13 -0300

    En el componente App.js utilizamos useState para registrar el estado de las citas; En el componente App.js importamos y utilizamos FlatList para recorrer y renderizar las citas de una forma mas optimizada(solo se carga lo que se ve en la pantalla); Creamos el componente Cita(componentes) para mostrar cada item de la coleccion de citas(recibida desde el componente App.js); Le damos estilo inicial al componente Cita(componentes) con los estilos personalizados cita(StyleSheet View), label(StyleSheet Text) y texto(StyleSheet Text).

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import Cita from './componentes/Cita';
 
 const App = () => {
+
+  //Definimos el state de las citas
+  const [ citas, setCitas ] = useState([
+    { id: "1", paciente: "Hook", propietario: "Juan", sintomas: "No Come" }, 
+    { id: "2", paciente: "Redux", propietario: "Pedro", sintomas: "No Duerme" },
+    { id: "3", paciente: "Native", propietario: "Maria", sintomas: "No Canta" }
+  ])
+
   return (
       <View style={styles.contenedor}>
-        <Text style={styles.titulo}>Proyecto Administrar Citas</Text>
+
+        <Text style={styles.titulo}>Administrador de Citas</Text>
+
+        <FlatList
+          data={citas}
+          renderItem={ ( { item } ) => <Cita item={item} /> }
+          keyExtractor={ cita => cita.id }
+        />
+
+        {/*citas.map(cita => (
+          <View> 
+            <Text>{cita.paciente}</Text>
+          </View>
+        ))*/}
+
       </View>
   );
 };
